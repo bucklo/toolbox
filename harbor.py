ede2733633a6fe2a3be95d8b47899b4f0b50e882
Simon Richardson <simon@richardson.nu>
2022-01-19 10:43:23 +0100

    initial

---
+++ harbor.py
@@ -0,0 +1,43 @@
+import requests
+from requests import auth
+from requests.auth import HTTPBasicAuth
+import json
+
+
+class harbor:
+    def __init__(self, provider):
+        host = provider["host"]
+        user = provider["user"]
+        pwd = provider["pwd"]
+
+        self.auth = HTTPBasicAuth(user, pwd)
+        self.headers = {"content-type": "application/json"}
+        self.baseURL = f"https://{host}/api/v2.0"
+
+    def getProjects(self):
+        url = f"{self.baseURL}/replication/policies"
+        response = requests.request(
+            "GET", url, headers=self.headers, auth=self.auth, verify=False
+        )
+        return json.loads(response.text)
+
+    def getRegistry(self, registry):
+        url = f"{self.baseURL}/registries/{registry}"
+        response = requests.request(
+            "GET", url, headers=self.headers, auth=self.auth, verify=False
+        )
+        return json.loads(response.text)
+
+    def createReplicationPolicy(self, policy):
+        url = f"{self.baseURL}/replication/policies"
+        response = requests.request(
+            "POST", url, headers=self.headers, auth=self.auth, verify=False, data=json.dumps(policy)
+        )
+        return json.loads(response.text)
+
+    def executeReplicationPolicy(self, policy_id):
+        url = f"{self.baseURL}/replication/executions"
+        response = requests.request(
+            "POST", url, headers=self.headers, auth=self.auth, verify=False, data=json.dumps(policy_id)
+        )
+        return json.loads(response.text)
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="toolbox",
+    version="0.0.1",
+    author="Simon Richardson",
+    author_email="simon@richardson.nu",
+    descriptioon="A collection of tools for use in my projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bucklo/toolbox",
+    packages=['toolbox'],
+    install_requires=[
+        'requests',
+        'pyVim',
+        'pyVmomi',
+        'ssl',
+        'json'
+    ],
