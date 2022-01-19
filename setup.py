62ea418804430c853b410f0ef4b57667cb362853
Simon Richardson <simon@richardson.nu>
2022-01-19 11:16:29 +0100

    fixed syntax error

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="toolbox",
-    version="0.0.2",
+    version="0.0.3",
     author="Simon Richardson",
     author_email="simon@richardson.nu",
     descriptioon="A collection of tools for use in my projects",
